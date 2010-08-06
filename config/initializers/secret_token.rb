0211c3e624996cf4dbb4f04c09cf6d7ca393a60d
David Cook <david@bloomfire.com>
2010-08-05 22:46:51 -0400

    initial empty rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photosbycook::Application.config.secret_token = '4814afc8ccd90581175222df7f3fb8088b49ebbd5a42bf599e7c32c78b1aec227155710a88d07cbeb69bb7009595a8f5de13dfa743c47affba7a1aae40b02cd6'
