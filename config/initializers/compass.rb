8068faf6f510d0c8b168428fe03851192587efe2
David Cook <david@bloomfire.com>
2010-08-08 23:22:00 -0400

    Push SASS update changes

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -6,4 +6,4 @@ Compass.configure_sass_plugin!
   
 #   Used to tell rack to look for stylesheets in tmp/stylesheets 
 # There is a problem with this in Heroku and the natively ready-only file structure
-ActionController::Dispatcher.middleware.use(Rack::Static, :root => “tmp/”, :urls => [“/stylesheets/compiled”])
+# ActionController::Dispatcher.middleware.use(Rack::Static, :root => “tmp/”, :urls => [“/stylesheets/compiled”])
